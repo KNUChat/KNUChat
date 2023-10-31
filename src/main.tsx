70695ede538594975957b5900290e5e1321e88cd
krokerdile <crazykroker@gmail.com>
2023-11-01 01:09:59 +0900

    feat: helmet 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import theme from "./style/theme.tsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <BrowserRouter basename="/KNUChat/">
-        <App />
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
--- src/page/Layout.tsx
+++ src/page/Layout.tsx
@@ -1,6 +1,11 @@
+import { Helmet } from "react-helmet-async";
+
 const Layout = () => {
   return (
     <>
+      <Helmet>
+        <title>KNUChat</title>
+      </Helmet>
       <div>Layout</div>
     </>
   );
