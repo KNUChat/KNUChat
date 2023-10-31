e4cd13062fbf4bea32978aa88879e75b45acdcab
krokerdile <crazykroker@gmail.com>
2023-10-31 14:04:05 +0900

    feat: github Action 용 baseUrl 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/KNUChat/",
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, "src") },
