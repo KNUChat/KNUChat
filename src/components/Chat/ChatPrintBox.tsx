590ebb78a1c96a65b3d54971667d62845b753eda
east_filmm <ehdvlf001@naver.com>
2023-11-26 19:39:48 +0900

    fix: 템플릿 적용 중

--- src/components/Chat/ChatPrintBox.tsx
+++ src/components/Chat/ChatPrintBox.tsx
@@ -1,9 +1,15 @@
+import styled from "styled-components";
+
 const ChatPrintBox = () => {
     return (
-      <>
+      <ChatPrintWrapper>
         
-      </>
+      </ChatPrintWrapper>
     );
   };
   
   export default ChatPrintBox;
+
+  const ChatPrintWrapper = styled.div`
+  
+  `;
