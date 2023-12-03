83f050012dcb4d20667d64c8436cfe5ea14faff9
krokerdile <crazykroker@gmail.com>
2023-12-03 09:34:04 +0900

    feat: 백엔드 api 명세서 맞춰서 수정

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,26 +1,65 @@
 import clientApi from "./axios";
+interface UserDto {
+  id: number;
+  name: string;
+  email: string;
+  gender: "MALE" | "FEMALE" | "OTHER"; // 수정 가능한 성별 옵션들
+}
 
+interface ProfileDto {
+  stdNum: number;
+  academicStatus: "ENROLLMENT" | "GRADUATION" | "SUSPENSION"; // 학적 상태 옵션들
+  graduateDate?: string; // 선택적으로 nullable한 값
+  admissionDate: string;
+  introduction: string;
+}
+
+interface DepartmentDto {
+  college: string;
+  major: string;
+  depCategory: "BASIC" | "MAJOR" | "MINOR"; // 학과 카테고리 옵션들
+}
+
+interface CertificationDto {
+  name: string;
+  achievement: string;
+  obtainDate: string;
+}
+
+interface UserDataProps {
+  userDto: UserDto;
+  profileDto: ProfileDto;
+  departmentDtos: DepartmentDto[];
+  certificationDtos: CertificationDto[];
+  urlDtos: string[];
+}
+
+interface UserSearchProps {
+  page: number;
+  major: string;
+}
 const userApi = {
   signIn: async () => {
-    return await clientApi.get("users/signin");
+    return await clientApi.user.get("users/signin");
   },
   signUp: async () => {
-    return await clientApi.post("/users/signup");
+    return await clientApi.user.post("/users/signup");
   },
-  initUserProfile: async () => {
-    return await clientApi.post("/users/");
+  initUserProfile: async (userData: UserDataProps) => {
+    return await clientApi.user.post("/users/", { userData });
   },
-  getUserProfile: async ({ id }: any) => {
-    return await clientApi.get(`/users/${id}`);
+  getUserProfile: async (queryKey: (string | number)[]) => {
+    const userId = queryKey[1];
+    return await clientApi.user.get(`/users/${userId}`);
   },
-  updateUserProfile: async () => {
-    return await clientApi.patch(`/users`);
+  updateUserProfile: async (userData: UserDataProps) => {
+    return await clientApi.user.patch("/users/", { userData });
   },
   deleteUserProfile: async () => {
-    return await clientApi.delete(`/users`);
+    return await clientApi.user.delete(`/users`);
   },
-  searchUserProfile: async ({ major }: any) => {
-    return await clientApi.get(`/users/search/${major}`);
+  searchUserProfile: async (searchData: UserSearchProps) => {
+    return await clientApi.user.get(`/users?page=${searchData.page}&major=${searchData.major}`);
   },
 };
 
