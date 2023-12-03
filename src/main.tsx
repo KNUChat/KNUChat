559e4e6e78542083d9da908fa9290a87ac3c56bc
krokerdile <crazykroker@gmail.com>
2023-12-03 09:33:05 +0900

    fix: react-query 이슈 때문에 임시로 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <BrowserRouter basename="/KNUChat/">
+        <BrowserRouter>
           <HelmetProvider>
             <App />
           </HelmetProvider>
