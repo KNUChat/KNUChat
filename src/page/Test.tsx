1eac3ff56561e34e0c724013463845cd54bdc2c2
krokerdile <crazykroker@gmail.com>
2023-12-03 09:33:44 +0900

    feat: backend 주소에 맞춰서 axios  나눠서 요청 가능하도록 변경

--- src/page/Test.tsx
+++ src/page/Test.tsx
@@ -2,9 +2,24 @@
 import useGetUserProfile from "@hook/user/useGetUserProfile";
 import styled from "styled-components";
 
+import axios from "axios";
+
 const Test = () => {
   const { data: userData } = useGetUserProfile();
+
   console.log(userData);
+
+  // Axios를 이용한 GET 요청
+  axios
+    .get("http://52.79.37.100:31338/users/1")
+    .then((response) => {
+      // 요청이 성공했을 때의 처리
+      console.log("응답 데이터:", response.data);
+    })
+    .catch((error) => {
+      // 요청이 실패했을 때의 처리
+      console.error("에러 발생:", error);
+    });
   return (
     <TestWrapper>
       <h1>User</h1>
